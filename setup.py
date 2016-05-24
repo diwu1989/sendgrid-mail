e6c8729143a688659d357b5d57765d39e1996d20
Di Wu <dw323@cornell.edu>
2016-05-24 00:46:26 -0700

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name='sendgrid-mail',
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='0.0.1',
 
     description='Command line utility to send mail via Sendgrid',
 
